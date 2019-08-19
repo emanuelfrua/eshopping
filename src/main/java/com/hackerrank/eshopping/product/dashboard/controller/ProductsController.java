4dd4394215d2580938b02f12014137c531669a0c
User <user@rba-hackerrank.com>
2019-08-19 00:22:51 +0000

    Auto commit on Run Tests
    Commit by HackerRank

--- src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
@@ -1,10 +1,20 @@
 package com.hackerrank.eshopping.product.dashboard.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductsController {
-
+    
+   
+	@RequestMapping(value = "/", method = RequestMethod.POST) 
+	public String createProduct() {
+	    return "201";
+	}
+	
 }
